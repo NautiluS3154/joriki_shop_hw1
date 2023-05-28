8bfcfd618f2e1e30af13f60850a7877b93c6719a
NautiluS3154 <talajbekovsultan@gmail.com>
2023-05-28 15:36:49 +0600

    дошлифовал и доделал

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './Button.css';
+import image from '../../assets/Button_img/shopping-cart.png'
 
 const Button = (props) =>{
     const {titleBtn} = props
     return(
-<button>
-  Button
-</button>
+        <button className="button">
+            {titleBtn}
+            <img src={image} alt="" className="btn_img" />
+        </button>
     )
 }
 export default Button;
